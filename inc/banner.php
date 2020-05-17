47db6484c20515a48e4ed7539b2a47090839e0b4
Wenke-D <65447561+Wenke-D@users.noreply.github.com>
2020-05-17 14:43:46 +0200

    basic built in index
    finish index basicly avec bootsrrap4

---
+++ inc/banner.php
@@ -0,0 +1,6 @@
+<div class="banner">
+    <img class="" src="<?php echo _IMG . 'logo111.png';?>" alt="logo">
+    <p class="banner-title">Talent Acquisition</p>
+    <p class="banner-text">For many years we have been helping companies to recruit and develop their talents.</p>
+    <button class="banner-button"><label class="banner-button-text">OUR SERVICES</label></button>
+</div>
