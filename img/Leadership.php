dce010c3c45f91305105cea28df1a85ab8576e60
Wenke-D <65447561+Wenke-D@users.noreply.github.com>
2020-05-19 14:35:59 +0200

    leardership-text-insertion

---
+++ img/Leadership.php
@@ -0,0 +1,135 @@
+<?php
+$curPage = "Leadership";
+$pageTitle = 'Leadership Development & Coaching';
+include "inc/head.php"
+?>
+    <div class="container">
+        <div class="leader-text-area1">
+            <p class="medium-red-titre  coaching-title">Executive coaching</p>
+            <p class="text">Executive coaching is a one-to-one interactive process between a middle- or top-level
+                professional (the “coachee”) and a certified coach. The general objective is to enable the coachee to
+                achieve rapid and long lasting results that are most valuable for him/her and the company. Executive
+                coaching is a process that is both very practical and efficient, and very respectful of each
+                individual.</p>
+        </div>
+        <ul>
+            <li>
+                <p class="item-title">Coaching in China</p>
+                <p class="text">Executive coaching is particularly useful in China because it also addresses the
+                    intercultural dimension of interpersonal relationships.
+                </p>
+                <ul class="text-list">
+                    <li>
+                        <span>Chinese managers need to develop their leadership to meet the expectations of their foreign
+                        peers and superiors and to be respected by their foreign subordinates.</span>
+
+                    </li>
+                    <li><span>Foreign managers need to balance their management style to interact more efficiently with their
+                            Chinese partners, peers and subordinates.</span>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <p class="item-title">When to use Executive Coaching?</p>
+                <p class="text">Coaching is particularly recommended in the following situations:</p>
+                <ul class="text-list">
+                    <li><span>To grow the most promising talents in your organization</span></li>
+                    <li><span>To solve tense situations between one individual and his subordinates, colleagues or superior</span>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <p class="item-title">Practical organization of our coaching</p>
+                <p class="text">
+                    With the help of the coach, the coachee will clarify the objective of the coaching, both for the
+                    company and for him.
+                </p>
+                <ul class="text-list">
+                    <li><span>Then the coachee and the coach will meet regularly for sessions of 90 or 120 min. The frequency is flexible and will depend on the need and availability of the coachee, but typically every 3 weeks. A coaching program spreads usually on 6 months, shorter or longer depending on the needs.</span>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <p class="item-title">Our Coaching Practice</p>
+                <p class="text"></p>
+                <ul class="text-list">
+                    <li><span>8 years corporate and business coaching in China since 2008</span></li>
+                    <li>
+                        <span>Repeat business includes retaining a corporate client for 4 consecutive years till today</span>
+                    </li>
+                    <li><span>Our coaches are professionally certified with years of coaching experience.</span></li>
+                    <li><span>Coaching can be done in several languages: Chinese (mandarin, Shanghainese, Cantonese), English, French, German and Italian.</span>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <p class="item-title">The efficiency of our Executive coaching</p>
+                <p class="text">Coaching is a very efficient tool for talent development because it is much more
+                    engaging and empowering than any training. It can be of great help for anyone, and particularly for
+                    Chinese people because their education and the corporate culture in Chinese companies do not foster
+                    self development.</p>
+                <ul class="text-list">
+                    <li><span></span></li>
+                    <li><span></span></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+    <div class="container">
+        <div class="leader-text-area2">
+            <p class="medium-red-titre  coaching-title">Team coaching</p>
+            <p class="text">Executive coaching is a one-to-one interactive process between a middle- or top-level
+                professional (the “coachee”) and a certified coach. The general objective is to enable the coachee to
+                achieve rapid and long lasting results that are most valuable for him/her and the company. Executive
+                coaching is a process that is both very practical and efficient, and very respectful of each
+                individual.</p>
+        </div>
+        <ul>
+            <li>
+                <p class="item-title">What is Team Coaching?</p>
+                <p class="text">Team coaching is a type of coaching that is applied to a group of people with a common
+                    goal. Its aim is to increase the team effectiveness and help achieve a desired outcome. Team size is
+                    usually between 5 and 20 members.</p>
+                <ul class="text-list">
+                    <li><span></span></li>
+                    <li><span></span></li>
+                </ul>
+            </li>
+            <li>
+                <p class="item-title">When to use Team Coaching?</p>
+                <p class="text">Team coaching is particularly suitable in the following situations:</p>
+                <ul class="text-list">
+                    <li>
+                        <span>To integrate two teams with different cultures (merger, acquisition, joint-venture)                            </span>
+                    </li>
+                    <li><span>To start-up a project team</span></li>
+                    <li><span>To boost a team (in terms of commitment, productivity, creativity, morale, etc.)</span>
+                    </li>
+                    <li><span>To re-mobilize teams after a restructuring or downsizing</span></li>
+                    <li><span>To embrace a new initiative or extraordinary challenging goals</span></li>
+                </ul>
+            </li>
+            <li>
+                <p class="item-title">Our Team coaching programs</p>
+                <p class="text">
+                    Team Reinforcement Program
+                    The key development issues to be addressed are defined before the workshop by the management. The
+                    workshop takes 1 or 2 days.
+
+                    Team DiagnosticTM Coaching
+
+                    This methodology was developed by Team Coaching International. Our coaches are accredited to perform
+                    this methodology in China. In this case, the key development issues are identified by the team
+                    during the process. The program begins with 1 or 2 days of workshop, followed by shorton-going
+                    sessions planned for several months to accompany the changes.
+
+                </p>
+                <ul class="text-list">
+                    <li><span></span></li>
+                    <li><span></span></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+<?php include("inc/foot.php"); ?>
---
+++ img/config/text.inc.php
@@ -0,0 +1,7 @@
+<?php
+define('_EXECUTIVE', 'Executive Search & Recruitment');
+define('_LEADERSHIP', 'Leadership Development & Coaching');
+define('_INTERCULTURAL', 'Intercultural Assessment & Facilitation');
+define('_SOFT', 'Soft Skills Development ');
+define('_INSIGHT', 'Insights & Resources');
+define('_ABOUT', 'About us');
